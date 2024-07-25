202b49611c1b868f619f3eb8939cc83185455fa0
oronila <noormehdiali@gmail.com>
2024-07-25 13:33:33 -0500

    user pool updates

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,9 +10,9 @@ Amplify.configure({
  Auth: {
    Cognito: {
      //  Amazon Cognito User Pool ID
-     userPoolId: 'us-east-1_ngn1sHNPR',
+     userPoolId: 'us-east-1_TkLDTNi2B',
      // OPTIONAL - Amazon Cognito Web Client ID (26-char alphanumeric string)
-     userPoolClientId: '70qevjjp7hvp31c839e4gldj29',
+     userPoolClientId: '6gljb1n6e47mcua7itafoigeag',
      // REQUIRED only for Federated Authentication - Amazon Cognito Identity Pool ID
      // identityPoolId: 'XX-XXXX-X:XXXXXXXX-XXXX-1234-abcd-1234567890ab',
      // OPTIONAL - Set to true to use your identity pool's unauthenticated role when user is not logged in
