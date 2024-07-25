31eb92b7941cfe4912dbf139204881044be879ac
oronila <noormehdiali@gmail.com>
2024-07-24 20:07:25 -0500

    basic auth setup

---
+++ src/app/pages/MainPage.tsx
@@ -0,0 +1,239 @@
+"use client";
+import Link from "next/link"
+import {
+  Bell,
+  CircleUser,
+  LineChart,
+  Menu,
+  Home as Homer,
+  Package,
+  Package2,
+  Search,
+  ShoppingCart,
+  Users,
+  Settings as SettingsIcon,
+} from "lucide-react"
+import { Badge } from "@/components/ui/badge"
+import { Button } from "@/components/ui/button"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+import { Input } from "@/components/ui/input"
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet"
+import logo from '../assets/fynopsis_noBG.png'
+import { useState } from "react"
+import Dashboard from "./Dashboard";
+import StockSearch from "./StockSearch";
+import Settings from "./Settings";
+import { useAuthenticator } from '@aws-amplify/ui-react';
+
+export default function Home() {
+
+  const [selectedTab, setSelectedTab] = useState("dashboard");
+  const { signOut } = useAuthenticator();
+
+  const renderSelectedScreen = () => {
+    switch (selectedTab) {
+      case "dashboard":
+        return <Dashboard />
+      case "stockSearch":
+        return <StockSearch />
+      case "settings":
+        return <Settings />
+      default:
+        return <Dashboard />
+    }
+  }
+
+  return (
+    <div className="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[250px_1fr] xl:grid-cols-[250px_1fr] 2xl:grid-cols-[350px_1fr]">
+      <div className="hidden border-r bg-muted/40 md:block">
+        <div className="flex h-full max-h-screen flex-col gap-2">
+          <div className="flex h-14 items-center border-b px-4 lg:h-[60px] lg:px-6 xl:h-[60px] 2xl:h-[90px]">
+            <Link href="/" className="flex items-center gap-2 font-semibold">
+            <img src={logo.src} alt="Fynopsis Logo" className="h-8 w-8 2xl:h-14 2xl:w-14" />
+
+              <span className="sm-text-lg lg:text-m 2xl:text-2xl">Iyan Sonesra</span>
+            </Link>
+            <Button variant="outline" size="icon" className="ml-auto h-8 w-8 2xl:[h-12 w-12]">
+              <Bell className="h-4 w-4 2xl:h-6 w-6" />
+              <span className="sr-only">Toggle notifications</span>
+            </Button>
+          </div>
+          <div className="flex-1">
+            <nav className="grid items-start px-2 lg:text-sm xl:text-sm 2xl:text-xl font-medium lg:px-4">
+            <Link
+              href="#"
+              className={`flex items-center gap-3 rounded-lg px-3 py-2 transition-all hover:text-primary ${
+                selectedTab === "dashboard" ? "bg-muted text-primary" : "text-muted-foreground"
+              }`}
+              onClick={() => setSelectedTab("dashboard")}
+            >
+              <Homer className="h-4 w-4" />
+              Dashboard
+            </Link>
+            <Link
+              href="#"
+              className={`flex items-center gap-3 rounded-lg px-3 py-2 transition-all hover:text-primary ${
+                selectedTab === "stockSearch" ? "bg-muted text-primary" : "text-muted-foreground"
+              }`}
+              onClick={() => setSelectedTab("stockSearch")}
+            >
+              <Search className="h-4 w-4" />
+              Stock Search
+            </Link>
+            <Link
+              href="#"
+              className={`flex items-center gap-3 rounded-lg px-3 py-2 transition-all hover:text-primary ${
+                selectedTab === "settings" ? "bg-muted text-primary" : "text-muted-foreground"
+              }`}
+              onClick={() => setSelectedTab("settings")}
+            >
+              <SettingsIcon className="h-4 w-4" />
+              Settings
+            </Link>
+            </nav>
+          </div>
+          <div className="mt-auto p-4">
+            <Card x-chunk="dashboard-02-chunk-0">
+              <CardHeader className="p-2 pt-0 md:p-4">
+                <CardTitle>Upgrade to Pro</CardTitle>
+                <CardDescription>
+                  Unlock all features and get unlimited access to our support
+                  team.
+                </CardDescription>
+              </CardHeader>
+              <CardContent className="p-2 pt-0 md:p-4 md:pt-0">
+                <Button size="sm" className="w-full">
+                  Upgrade
+                </Button>
+              </CardContent>
+            </Card>
+          </div>
+        </div>
+      </div>
+      <div className="flex flex-col">
+        <header className="flex h-14 items-center gap-4 border-b bg-muted/40 px-4 lg:h-[60px] lg:px-6 xl:h-[60px] 2xl:h-[90px]">
+          <Sheet>
+            <SheetTrigger asChild>
+              <Button
+                variant="outline"
+                size="icon"
+                className="shrink-0 md:hidden"
+              >
+                <Menu className="h-5 w-5" />
+                <span className="sr-only">Toggle navigation menu</span>
+              </Button>
+            </SheetTrigger>
+            <SheetContent side="left" className="flex flex-col">
+              <nav className="grid gap-2 text-lg font-medium">
+                <Link
+                  href="#"
+                  className="flex items-center gap-2 text-lg font-semibold"
+                >
+                  <Package2 className="h-6 w-6" />
+                  <span className="sr-only">Acme Inc</span>
+                </Link>
+                <Link
+                  href="#"
+                  className="mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2 text-muted-foreground hover:text-foreground"
+                >
+                  {/* <Home className="h-5 w-5" /> */}
+                  Dashboard
+                </Link>
+                <Link
+                  href="#"
+                  className="mx-[-0.65rem] flex items-center gap-4 rounded-xl bg-muted px-3 py-2 text-foreground hover:text-foreground"
+                >
+                  <ShoppingCart className="h-5 w-5" />
+                  Orders
+                  <Badge className="ml-auto flex h-6 w-6 shrink-0 items-center justify-center rounded-full">
+                    6
+                  </Badge>
+                </Link>
+                <Link
+                  href="#"
+                  className="mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2 text-muted-foreground hover:text-foreground"
+                >
+                  <Package className="h-5 w-5" />
+                  Products
+                </Link>
+                <Link
+                  href="#"
+                  className="mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2 text-muted-foreground hover:text-foreground"
+                >
+                  <Users className="h-5 w-5" />
+                  Customers
+                </Link>
+                <Link
+                  href="#"
+                  className="mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2 text-muted-foreground hover:text-foreground"
+                >
+                  <LineChart className="h-5 w-5" />
+                  Analytics
+                </Link>
+              </nav>
+              <div className="mt-auto">
+                <Card>
+                  <CardHeader>
+                    <CardTitle>Upgrade to Pro</CardTitle>
+                    <CardDescription>
+                      Unlock all features and get unlimited access to our
+                      support team.
+                    </CardDescription>
+                  </CardHeader>
+                  <CardContent>
+                    <Button size="sm" className="w-full">
+                      Upgrade
+                    </Button>
+                  </CardContent>
+                </Card>
+              </div>
+            </SheetContent>
+          </Sheet>
+          <div className="w-full flex-1">
+            <form>
+              <div className="relative">
+                <div className = "flex flex-col gap-0">
+                    <h1 className = "font-semibold text-med mb-0 2xl:text-2xl">Hello, Iyan!</h1>
+                    <h1 className = "text-sm -mt-1 text-gray-700 2xl:text-lg">Tuesday, July 6th, 2024</h1>
+                </div>
+              </div>
+            </form>
+          </div>
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button variant="secondary" size="icon" className="rounded-full">
+                <CircleUser className="h-5 w-5 2xl:h-8 2xl:w-8" />
+                <span className="sr-only">Toggle user menu</span>
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="end">
+              <DropdownMenuLabel>My Account</DropdownMenuLabel>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem>Settings</DropdownMenuItem>
+              <DropdownMenuItem>Support</DropdownMenuItem>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem onClick={signOut}>Logout</DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        </header>
+        <main className="bg-slate-50 flex-grow p-0 overflow-auto">
+        {renderSelectedScreen()}
+        </main>
+      </div>
+    </div>
+  );
+}
---
+++ src/components/Authenticator.tsx
@@ -0,0 +1,127 @@
+import { PropsWithChildren } from "react";
+import {
+ Authenticator as AmplifyAuthenticator,
+ ThemeProvider as AmplifyThemeProvider,
+ Theme,
+} from "@aws-amplify/ui-react";
+import "@aws-amplify/ui-react/styles.css";
+import { Amplify } from "aws-amplify";
+
+// const userPoolConfig = Amplify.getConfig();
+
+// Amplify.configure({
+//     Auth: {
+//       Cognito: {
+//         loginWith: {
+//           oauth: {
+//             redirectSignIn: [
+//               'http://localhost:3000/'
+//             ],
+//             redirectSignOut: [
+//               'http://localhost:3000/'
+//             ]
+//           }
+//         },
+//         ...userPoolConfig
+//       }
+//     }
+// });
+
+export const Authenticator: React.FC<PropsWithChildren> = ({ children }) => {
+ const theme: Theme = {
+   name: "bookshelf",
+   tokens: {
+     colors: {
+       font: {
+         secondary: { value: "{colors.brand.primary.90}" },
+       },
+       brand: {
+         primary: {
+           10: { value: "{colors.overlay.10}" },
+           20: { value: "{colors.overlay.20}" },
+           40: { value: "{colors.overlay.40}" },
+           60: { value: "{colors.overlay.60}" },
+           80: { value: "{colors.overlay.90}" },
+           90: { value: "{colors.black}" },
+           100: { value: "{colors.black}" },
+         },
+         secondary: {
+           10: { value: "{colors.neutral.10}" },
+           20: { value: "{colors.neutral.20}" },
+           40: { value: "{colors.neutral.40}" },
+           60: { value: "{colors.neutral.60}" },
+           80: { value: "{colors.neutral.80}" },
+           90: { value: "{colors.neutral.90}" },
+           100: { value: "{colors.neutral.100}" },
+         },
+       },
+     },
+     radii: {
+       small: { value: "0.75rem" },
+     },
+     components: {
+       authenticator: {
+         modal: {
+           backgroundColor: { value: "{colors.blue.40}" },
+         },
+         router: {
+           borderWidth: { value: "0" },
+         },
+         state: {
+           inactive: {
+             backgroundColor: { value: "{colors.brand.primary.100}" },
+           },
+         },
+       },
+       tabs: {
+         item: {
+           borderColor: { value: "{colors.brand.primary.100}" },
+           color: { value: "{colors.brand.secondary.20}" },
+           _active: {
+             backgroundColor: { value: "{colors.brand.primary.100}" },
+             color: { value: "{colors.brand.secondary.10}" },
+             borderColor: { value: "{colors.brand.secondary.40}" },
+           },
+           _hover: {
+             color: { value: "{colors.brand.secondary.40}" },
+           },
+           _focus: {
+             color: { value: "{colors.brand.secondary.40}" },
+           },
+         },
+       },
+     },
+   },
+ };
+
+
+ return (
+   <AmplifyThemeProvider theme={theme}>
+    {/* socialProviders={['google']} */}
+     <AmplifyAuthenticator 
+       variation="modal"
+       signUpAttributes={["name"]}
+       formFields={{
+         signIn: {
+           username: {
+             label: "Email",
+             placeholder: "Enter your email",
+           },
+         },
+         signUp: {
+           username: {
+             label: "Email",
+             placeholder: "Enter your email",
+           },
+           name: {
+             label: "First name",
+             placeholder: "Enter your first name",
+           },
+         },
+       }}
+     >
+       {children}
+     </AmplifyAuthenticator>
+   </AmplifyThemeProvider>
+ );
+};
